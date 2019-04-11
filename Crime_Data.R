db3eb823bdc0bd5cecdb011c9526cb430bcc080a
unknown <L00144762@student.lyit.ie>
2019-04-11 17:13:54 +0100

    second commit - few more bits added to crime data

--- Crime_Data.R
+++ Crime_Data.R
@@ -16,12 +16,13 @@ csv_files
 
 #creating a dataframe consisting of each all the csv files
 
-AllNICrimeData <- do.call(rbind.data.frame,lapply(csv_files, 
+AllNICrimeData <- do.call(rbind.data.frame, lapply(csv_files, 
                                            FUN=function(amalgamate){ 
                                              
                                              read.csv(amalgamate)
                                              
                                              }))
+
 
 #changing working directory back to project
 
@@ -29,5 +30,33 @@ setwd("C:/Users/Richard/Documents/CA2_Crime_Data/")
 
 write.csv(AllNICrimeData, "AllNICrimeData.csv")
 
+#AllNICrimeData[AllNICrimeData == ""] <- NA
+
 #number of rows in dataset
 nrow(AllNICrimeData)
+
+#tidying dataset; seeing empty cols and getting rid of empty colus
+
+x<- colSums(is.na(AllNICrimeData)) #total na per col
+
+y <- nrow(AllNICrimeData) #total row per col
+
+y-x  # result: any that equal zero are columns containing nothing bu NA and so can be deleted
+     #crime id is a string of 
+
+AllNICrimeData <- subset(AllNICrimeData, select = -c(Crime.ID, LSOA.code, LSOA.name,
+                                                             Last.outcome.category, Context)) 
+
+# factorising Crime type attribute
+AllNICrimeData$Crime.type <- factor(AllNICrimeData$Crime.type)
+str(AllNICrimeData$Crime.type)
+
+
+#celaning Location attribute
+AllNICrimeData$Location <- gsub("On or near", "", AllNICrimeData$Location)
+AllNICrimeData$Location[AllNICrimeData$Location == " "] <- NA
+
+
+
+
+
